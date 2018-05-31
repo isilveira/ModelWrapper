a014b805c163288ef95386c6741fbd6bed5ea763
isilveira <italobrian@gmail.com>
2018-05-30 23:17:44 -0300

    Project structures

---
+++ Source/ModelWrapper/Core/IWrap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelWrapper.Core
+{
+    interface IWrap<TModel> where TModel: class
+    {
+        void Set(TModel model);
+        TModel Put(TModel model);
+        TModel Patch(TModel model);
+
+        Dictionary<string, object> AsDictionary();
+    }
+}
---
+++ Source/ModelWrapper/Wrap.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ModelWrapper.Core;
+
+namespace ModelWrapper
+{
+    public class Wrap<T> : IWrap<T> where T : class
+    {
+        private Dictionary<string, object> Attributes;
+
+        static Wrap() { }
+        public Wrap() { }
+        public Wrap(Dictionary<string, object> attributes)
+        {
+            Attributes = attributes;
+        }
+
+        public Dictionary<string, object> AsDictionary()
+        {
+            return Attributes;
+        }
+
+        public T Patch(T model)
+        {
+            Attributes.ToList().ForEach(attribute =>
+                model.GetType().GetProperties().Where(x => x.Name.ToLower().Equals(attribute.Key.ToLower())).SingleOrDefault()
+                    .SetValue(model, attribute.Value));
+            return model;
+        }
+
+        public T Put(T model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Set(T model)
+        {
+            model.GetType().GetProperties().ToList().ForEach(property => Attributes.Add(property.Name.ToLower(), property.GetValue(model)));
+        }
+    }
+}
