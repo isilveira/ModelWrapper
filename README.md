b137fc41a6fce7f785b210b89eff8ac1ae5d45d9
isilveira <italobrian@gmail.com>
2018-06-27 21:56:30 -0300

    Versão: 1.0.0-beta Version: 1.0..0-beta

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # ModelWrapper
 
 Model wrapper - Tool wich enrich the experience of data models manipulation in RestFul APIs for C# WebApi projects.
+____
+
+Nuget
+----
+https://www.nuget.org/packages/ModelWrapper
+
+Nuget CLI
+PM> Install-Package ModelWrapper -Version 1.0.0-beta 
--- Source/ModelWrapper/Wrap.cs
+++ Source/ModelWrapper/Wrap.cs
@@ -12,55 +12,100 @@ namespace ModelWrapper
     public class Wrap<TModel> : DynamicObject, IWrap<TModel>
         where TModel : class
     {
+        #region Properties
+        /// <summary>
+        /// Internal model object that hold the supplied value.
+        /// </summary>
         private TModel InternalObject { get; set; }
+
+        /// <summary>
+        /// List of properties of the model wrapped.
+        /// </summary>
         private List<PropertyInfo> AllProperties { get; set; }
+
+        /// <summary>
+        /// List of supplied properties of the model wrapped.
+        /// </summary>
         private List<PropertyInfo> SuppliedProperties { get; set; }
 
+        /// <summary>
+        /// Dictionary with all supplied data.
+        /// </summary>
         private Dictionary<string, object> Attributes;
+        #endregion
 
-        static Wrap() { }
-
-        private void InitializePrivateObjects()
-        {
-            InternalObject = Activator.CreateInstance<TModel>();
-            SuppliedProperties = new List<PropertyInfo>();
-            AllProperties = typeof(TModel).GetProperties().ToList();
-            Attributes = new Dictionary<string, object>();
-        }
-
+        #region Contructors
+        /// <summary>
+        /// Wrap constructor without parameters
+        /// </summary>
         public Wrap()
         {
             InitializePrivateObjects();
         }
+
+        /// <summary>
+        /// Wrap constructor that recieves a dictionary.
+        /// </summary>
+        /// <param name="attributes">Dictionary with values to create a wrapper</param>
         public Wrap(Dictionary<string, object> attributes)
         {
             InitializePrivateObjects();
             Attributes = attributes;
             Attributes.ToList().ForEach(pair => SetPropertyValue(pair));
         }
-
+        #endregion
+        
+        #region Wrapper public methods
+        /// <summary>
+        /// Method that deliver the dictionary of wrapper.
+        /// </summary>
+        /// <returns>Dictionary with values of wrapper</returns>
         public Dictionary<string, object> AsDictionary()
         {
             return Attributes;
         }
 
+        /// <summary>
+        /// Method that patch data into the model object
+        /// </summary>
+        /// <param name="model">Model object</param>
+        /// <returns>Return model object</returns>
         public TModel Patch(TModel model)
         {
             SuppliedProperties.ForEach(property => property.SetValue(model, property.GetValue(InternalObject)));
             return model;
         }
 
+        /// <summary>
+        /// Method that put data into the model object
+        /// </summary>
+        /// <param name="model">Model object</param>
+        /// <returns>Return model object</returns>
         public TModel Put(TModel model)
         {
             AllProperties.ForEach(property => property.SetValue(model, property.GetValue(InternalObject)));
             return model;
         }
+        #endregion
 
+        #region Access member methods
+        /// <summary>
+        /// Mothod that overrides the access member get
+        /// </summary>
+        /// <param name="binder">GetMemberBinder object</param>
+        /// <param name="result">Value that will be returned</param>
+        /// <returns>bool, if succeeds true</returns>
         public override bool TryGetMember(GetMemberBinder binder, out object result)
         {
             return Attributes.TryGetValue(binder.Name, out result);
         }
 
+        /// <summary>
+        /// Mothod that overrides the access member set
+        /// </summary>
+        /// <param name="binder">SetMemberBinder object</param>
+        /// <param name="value">Value that will be set</param>
+        /// <returns>bool, if succeeds true</returns>
         public override bool TrySetMember(SetMemberBinder binder, object value)
         {
             Attributes.Add(binder.Name, value);
@@ -69,7 +114,24 @@ namespace ModelWrapper
 
             return true;
         }
+        #endregion
 
+        #region Wrapper private methods
+        /// <summary>
+        /// Method that initialize all private objects.
+        /// </summary>
+        private void InitializePrivateObjects()
+        {
+            InternalObject = Activator.CreateInstance<TModel>();
+            SuppliedProperties = new List<PropertyInfo>();
+            AllProperties = typeof(TModel).GetProperties().ToList();
+            Attributes = new Dictionary<string, object>();
+        }
+
+        /// <summary>
+        /// Method used to set property value
+        /// </summary>
+        /// <param name="token">Dictionary key value pair</param>
         private void SetPropertyValue(KeyValuePair<string, object> token)
         {
             var property = typeof(TModel).GetProperties().SingleOrDefault(p => p.Name.ToLower().Equals(token.Key.ToLower()));
@@ -80,6 +142,7 @@ namespace ModelWrapper
                 var newPropertyValue = (token.Value is JToken) ? JsonConvert.DeserializeObject(token.Value.ToString(), property.PropertyType) : Convert.ChangeType(token.Value, property.PropertyType);
                 property.SetValue(this.InternalObject, newPropertyValue);
             }
-        }
+        } 
+        #endregion
     }
 }
