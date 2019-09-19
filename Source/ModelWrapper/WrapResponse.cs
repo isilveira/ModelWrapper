a5f6f4d7cfab0792c77895a71184678f97c0331f
Ítalo Silveira <italobrian@gmail.com>
2019-09-19 01:06:39 -0300

    Update WrapResponse.cs

--- Source/ModelWrapper/WrapResponse.cs
+++ Source/ModelWrapper/WrapResponse.cs
@@ -8,32 +8,19 @@ namespace ModelWrapper
     public class WrapResponse<TModel> : Dictionary<string, object>
         where TModel : class
     {
-        //public string Message { get; set; }
-        //public long ResultCount { get; set; }
-        //public Dictionary<string, object> Request { get; set; }
-        //public Dictionary<string, object> Data { get; set; }
         public WrapResponse() { }
         public WrapResponse(WrapRequest<TModel> request, TModel data, string message = null, long? resultCount = null)
         {
-            Initialize();
             if (!string.IsNullOrWhiteSpace(message))
             {
-                //Message = message;
                 Add(nameof(message), message);
             }
             if (resultCount.HasValue)
             {
-                //ResultCount = resultCount.Value;
                 Add(nameof(resultCount), resultCount);
             }
-            //Request = request.GetRequestAsDictionary();
             Add(nameof(request), request.GetRequestAsDictionary());
             Add(nameof(data), data);
-        }
-
-        private void Initialize()
-        {
-            //Request = new Dictionary<string, object>();
         }
     }
 }
